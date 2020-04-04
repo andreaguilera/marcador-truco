5924093b0112eaaf0b6a6d71bdae64c12c3bb578
Andre Aguilera <andre.aguilera55@hotmail.com>
2020-04-04 12:43:44 -0300

    Added a banner test adMob and modifications on reset button

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import { reset } from 'expo/build/AR';
+import { AdMobBanner, setTestDeviceIDAsync } from 'expo-ads-admob'
 
 export default function App() {
   const [wePoints, setWePoints] = useState(0)
@@ -18,50 +19,57 @@ export default function App() {
     setTheyPoints(0)
   }
 
-  function resetWe(){
-    setWePoints(0)
-    setWeTotal(0)
-  }
-
-  function resetThey(){
+  function resetAll(){
     setTheyPoints(0)
     setTheyTotal(0)
+    setWePoints(0)
+    setWeTotal(0)
   }
 
 
   return (
     <View style={styles.container}>
-      <View style={styles.sides}>
-        <Text style={styles.team}>NÓS</Text>
-        <Text style={styles.points}>{wePoints}</Text>
-        <Text style={styles.team}>TOTAL</Text>
-        <Text style={styles.points}>{weTotal}</Text>
-        <TouchableOpacity style={styles.button} onPress={ ()=> {setWePoints(wePoints + 1)} }>
-          <Text style={styles.btnText}>+1</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.button} onPress={()=>{ setWePoints(wePoints + 3) }}>
-          <Text style={styles.btnText}>+3</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.button} onPress={()=>{ resetWe() }}>
-          <Text style={styles.btnText}>RESET</Text>
-          </TouchableOpacity>
+      <View style={styles.container2}>
+        <View style={styles.sides}>
+          <Text style={styles.team}>NÓS</Text>
+          <Text style={styles.points}>{wePoints}</Text>
+          <Text style={styles.team}>TOTAL</Text>
+          <Text style={styles.points}>{weTotal}</Text>
+          <TouchableOpacity style={styles.button} onPress={ ()=> {setWePoints(wePoints + 1)} }>
+            <Text style={styles.btnText}>+1</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.button} onPress={()=>{ setWePoints(wePoints + 3) }}>
+            <Text style={styles.btnText}>+3</Text>
+            </TouchableOpacity>
+        </View>
+
+        <View style={styles.sides}>
+          <Text style={styles.team}>ELES</Text>
+          <Text style={styles.points}>{theyPoints}</Text>
+          <Text style={styles.team}>TOTAL</Text>
+          <Text style={styles.points}>{theyTotal}</Text>
+          <TouchableOpacity style={styles.button} onPress={ () => { setTheyPoints(theyPoints + 1)} }>
+            <Text style={styles.btnText}>+1</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.button} onPress={()=>{ setTheyPoints(theyPoints + 3) }}>
+            <Text style={styles.btnText}>+3</Text>
+            </TouchableOpacity>         
+        </View>
       </View>
 
-      <View style={styles.sides}>
-        <Text style={styles.team}>ELES</Text>
-        <Text style={styles.points}>{theyPoints}</Text>
-        <Text style={styles.team}>TOTAL</Text>
-        <Text style={styles.points}>{theyTotal}</Text>
-        <TouchableOpacity style={styles.button} onPress={ () => { setTheyPoints(theyPoints + 1)} }>
-          <Text style={styles.btnText}>+1</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.button} onPress={()=>{ setTheyPoints(theyPoints + 3) }}>
-          <Text style={styles.btnText}>+3</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.button} onPress={()=>{ resetThey() }}>
-          <Text style={styles.btnText}>RESET</Text>
-          </TouchableOpacity>
-      </View>
+      <TouchableOpacity style={styles.resetBtn} onPress={()=>{ resetAll() }}>
+            <Text style={styles.btnText}>RESET</Text>
+            </TouchableOpacity> 
+
+            <AdMobBanner
+              style={styles.banner}
+              bannerSize="fullBanner"
+              adUnitID="ca-app-pub-3940256099942544/6300978111"
+              setTestDeviceIDAsync
+              servePersonalizedAds // true or false
+              onDidFailToReceiveAdWithError={ err => console.log( err ) }
+            />
+      
     </View>
   );
 }
@@ -69,6 +77,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#363636',
+  },
+  container2: {
     backgroundColor: '#363636',
     flexDirection: 'row',
   },
@@ -108,5 +119,24 @@ const styles = StyleSheet.create({
   btnText: {
     fontWeight: 'bold',
     fontSize: 25
+  },
+  resetBtn: {
+    backgroundColor: '#808080',
+    borderRadius: 8,
+    height: 50,
+    maxWidth: 330,
+    marginLeft: 15,
+    alignItems: 'center',
+    justifyContent: 'center',
+    shadowColor: "#000",
+    shadowOffset: {
+	  width: 10,
+    height: 7,    
+    },
+    shadowRadius: 9.11,
+    elevation: 14,
+  },
+  banner: {
+    marginTop: 40
   }
 });
